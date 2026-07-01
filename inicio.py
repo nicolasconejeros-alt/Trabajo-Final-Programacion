667d7bedc0ef134ca695fbba5f724260863ad0b5
nicolasconejeros-alt <nicolasconejerosotarola@gmail.com>
2026-07-01 16:10:08 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Titulo del proyecto")
 st.image("https://img.magnific.com/vector-premium/configuracion-moderna-computadoras-escritorio-productividad-creativa_1089479-30424.jpg?semt=ais_hybrid&w=740&q=80")
